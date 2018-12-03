7de9def30ee4fc1532563ade7993daa1ba08e06b
Ianeselli Caterina <38403782+icate95@users.noreply.github.com>
2018-12-03 17:55:57 +0100

    update license

--- tnt.php
+++ tnt.php
@@ -6,6 +6,8 @@
  * Version: 1.0.0
  * Author: Scuola Mohole
  * Author URI: https://scuola.mohole.it
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
 // termino l'esecuzione se il plugin è richiamato direttamente
